452cd9e1016846f128370dedb496a557af6e0067
Kaitokid0929 <skaito854@gmail.com>
2024-10-09 16:17:51 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error("MongoDB connection error:", err));
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
